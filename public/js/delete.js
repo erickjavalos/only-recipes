9bd6d064b0124708293a584d1b3dcb9ea893111a
eavalo14 <avaloserick97@gmail.com>
2023-02-05 12:36:42 -0500

    added delete functionality for a specific recipe

--- public/js/delete.js
+++ public/js/delete.js
@@ -1,18 +1,22 @@
-const logout = async () => {
+const deleteId = async () => {
     console.log('deleting')
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const id = urlParams.get('id')
+    console.log(id);
     // execute logout procedure in backend
-    // const response = await fetch('/api/users/logout', {
-    //   method: 'POST',
-    //   headers: { 'Content-Type': 'application/json' },
-    // });
+    const response = await fetch('/api/users/' + id, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+    });
   
-    // if (response.ok) {
-    //   document.location.replace('/');
-    // } else {
-    //   alert('Failed to log out.');
-    // }
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log out.');
+    }
   };
   
   // event listener on logout button press
-  document.querySelector('#delete').addEventListener('click', logout);
+  document.querySelector('#delete').addEventListener('click', deleteId);
   
