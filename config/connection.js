54e7cab045bc6aca268fcbdb0e430c70cbae7de9
eavalo14 <avaloserick97@gmail.com>
2023-02-06 00:54:42 -0500

    adding logic

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,17 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+// instantiate sequelize class for synchronizing with db
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
+  console.log('entered')
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PW,
+    process.env.DB_PASSWORD,
     {
       host: 'localhost',
       dialect: 'mysql',
@@ -17,3 +19,6 @@ if (process.env.JAWSDB_URL) {
     },
   );
 }
+
+
+module.exports = sequelize;
