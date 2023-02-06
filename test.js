17882b7249e4690d4ef182e21c973602b8b817b5
eavalo14 <avaloserick97@gmail.com>
2023-02-05 19:29:29 -0500

    adding updates to use ipfs hash in html rendering

---
+++ test.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const fs = require('fs')
+var FormData = require('form-data');
+
+require('dotenv').config();
+
+const formData = new FormData();
+formData.append("file", fs.createReadStream('./image6.png'))
+axios({
+	method: 'post',
+	url: 'https://api.pinata.cloud/pinning/pinFileToIPFS',
+	data: formData,
+	headers:{
+		"Authorization": `Bearer ${process.env.ACCESS_TOKEN}`
+	}
+  }).then((response)=>{
+		const ImgHash = `ipfs://${response.data.IpfsHash}`;
+		console.log(ImgHash); 
+  });
